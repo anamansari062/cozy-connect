30e156e49f98f8aeaefd39abf2e5310e5b576d18
Saviour1001 <harshghodkar@gmail.com>
2023-02-04 21:47:53 +0530

    solved anam's error, saviour to the rescue

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import { Inter, Tenor_Sans } from '@next/font/google'
+import Head from "next/head";
+import { Inter, Tenor_Sans } from "@next/font/google";
 import { useContext, useState } from "react";
 import { Textarea, Grid, useInput, Spacer, Button } from "@nextui-org/react";
-import Link from 'next/link'
-import getEmotionsFromSentence from '@/controllers/openaiController';
+import Link from "next/link";
+import getEmotionsFromSentence from "@/controllers/openaiController";
 // const getEmotionsFromSentence = '@/controllers/openaiController'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [ thoughts, setThoughts ] = useState();
+  const [thoughts, setThoughts] = useState();
   return (
     <>
       <Head>
@@ -19,36 +19,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h2 >*Add Video Screen Here*</h2>
-        <Spacer y={2}/>
-        <div >
-           <Textarea 
+        <h2>*Add Video Screen Here*</h2>
+        <Spacer y={2} />
+        <div>
+          <Textarea
             minRows={10}
             maxRows={12}
             width={800}
-            labelPlaceholder="Write your thoughts and feelings" 
+            labelPlaceholder="Write your thoughts and feelings"
             size="xl"
             color="success"
-            />
+            id="thoughts"
+          />
+          <Spacer y={0.5} />
+          <div className="relative inline-block px-4 py-2 font-medium group ">
+            <button
+              className="relative inline-block px-4 py-2 font-medium group "
+              onClick={async () => {
+                await getEmotionsFromSentence(
+                  document.getElementById("thoughts").value
+                );
+              }}
+            >
+              <span className="absolute rounded-lg inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-tertiary border-[2px] border-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute rounded-lg inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-tertiary"></span>
+              <span className="relative text-black">Find Matches</span>
+            </button>
             <Spacer y={0.5} />
-            <div  className="relative inline-block px-4 py-2 font-medium group ">
-              <button
-                className="relative inline-block px-4 py-2 font-medium group "
-                onClick={async() => {
-                  await getEmotionsFromSentence("Hi")
-                }}
-              >
-                <span className="absolute rounded-lg inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-tertiary border-[2px] border-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute rounded-lg inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-tertiary"></span>
-                <span className="relative text-black">Find Matches</span>
-              </button>
-              <Spacer y={0.5}/>
-              <h2 className="text-primary">
-                We will find a video call match for you. You will have 5 minutes max to have a conversation with them.
-              </h2>
-            </div>
+            <h2 className="text-primary">
+              We will find a video call match for you. You will have 5 minutes
+              max to have a conversation with them.
+            </h2>
+          </div>
         </div>
       </main>
     </>
-  )
+  );
 }
